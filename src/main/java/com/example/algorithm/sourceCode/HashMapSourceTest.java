22aca3a5343b29bdee3fe8e3fe8b3f67e68f1830
xuemingming <xuemingming@zuowenzhitiao.com>
2020-12-20 12:25:23 +0800

    '添加多线程demo'

--- src/main/java/com/example/algorithm/sourceCode/HashMapSourceTest.java
+++ src/main/java/com/example/algorithm/sourceCode/HashMapSourceTest.java
@@ -13,8 +13,23 @@ public class HashMapSourceTest {
      */
 
     public static void test1(){
+
         HashMap<Integer,Integer> map = new HashMap<>(16);
         Integer i = map.put(1,2);
+
+        Integer i2 = map.put(1,222);
         System.out.println(i);
+        int tableSizeFor = tableSizeFor(10);
+        System.out.println(tableSizeFor);
+    }
+
+    static int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= 1 << 30) ? 1 << 30 : n + 1;
     }
 }
