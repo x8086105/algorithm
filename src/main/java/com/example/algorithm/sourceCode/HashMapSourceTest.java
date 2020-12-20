64680e1cdb2536c658ddc829a3574658f47cb4e9
xuemingming-home <1090130448@qq.com>
2020-12-20 12:27:23 +0800

    'fixbug'

--- src/main/java/com/example/algorithm/sourceCode/HashMapSourceTest.java
+++ src/main/java/com/example/algorithm/sourceCode/HashMapSourceTest.java
@@ -4,8 +4,15 @@ import java.util.HashMap;
 
 public class HashMapSourceTest {
 
+    private static final int MAXIMUM_CAPACITY = 1<<30;
+
     public static void main(String[] args) {
         test1();
+//        int c = tableSizeFor(18);
+//        System.out.println(c);
+        System.out.println("ABCDEa123abc".hashCode());
+
+        System.out.println("ABCDFB123abc".hashCode());
     }
 
     /**
@@ -13,8 +20,40 @@ public class HashMapSourceTest {
      */
 
     public static void test1(){
-        HashMap<Integer,Integer> map = new HashMap<>(16);
-        Integer i = map.put(1,2);
-        System.out.println(i);
+        HashMap<String,Integer> map = new HashMap<>();
+        for (int i = 0; i < 20; i++) {
+            map.put(i + "", i);
+        }
+
+    }
+
+    static int tableSizeFor(int cap) {
+        int n = cap - 1;
+        int n1 = n>>>1;
+        System.out.println("n1:" + n1);
+        int result1 = n | n1;
+        System.out.println("result1:" + result1);
+        n |= n >>> 1;
+        int n2 = n>>>2;
+        System.out.println("n2:" + n2);
+        int result2 = n | n2;
+        System.out.println("result2:" + result2);
+        n |= n >>> 2;
+        int n3 = n>>>4;
+        System.out.println("n3:" + n3);
+        int result3 = n | n3;
+        System.out.println("result3:" + result3);
+        n |= n >>> 4;
+        int n4 = n>>>8;
+        System.out.println("n4:" + n4);
+        int result4 = n | n4;
+        System.out.println("result4:" + result4);
+        n |= n >>> 8;
+        int n5 = n>>>16;
+        System.out.println("n5:" + n5);
+        int result5 = n | n5;
+        System.out.println("result5:" + result5);
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
     }
 }
