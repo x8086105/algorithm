fbb5f0a6da79fd64bd6434f0048c38386bd289c0
xuemingming <xuemingming@zuowenzhitiao.com>
2021-01-05 21:35:44 +0800

    '添加多人聊天室升级版'

---
+++ src/main/java/com/example/socket/mutileChat/ChatClient.java
@@ -0,0 +1,155 @@
+package com.example.socket.mutileChat;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.platform.commons.util.StringUtils;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Set;
+
+@Slf4j
+public class ChatClient {
+    /***
+     * 服务器默认的地址
+     */
+    private static final String DEFAULT_SERVER_HOST = "127.0.0.1";
+    /**
+     * 服务器默认的端口号
+     */
+    private static final int DEFAULT_SERVER_PORT = 8888;
+    /**
+     * 中止通话的字符串
+     */
+    private static final String QUIT = "quit";
+    /**
+     * 默认缓存的大小
+     */
+    private static final int BUFFER = 1024;
+    /**
+     * 客户端的channel
+     */
+    private SocketChannel client;
+    /**
+     * 选择器
+     */
+    private Selector selector;
+    /**
+     * 选择器，也称为多路复用
+     */
+    private ByteBuffer rBuffer = ByteBuffer.allocate(BUFFER);
+
+    private ByteBuffer wBuffer = ByteBuffer.allocate(BUFFER);
+
+    private Charset charset = Charset.forName("UTF-8");
+
+    private String host;
+    private int port;
+
+    public ChatClient(){
+        this(DEFAULT_SERVER_HOST,DEFAULT_SERVER_PORT);
+    }
+
+    public ChatClient(String host,int port){
+        this.host = host;
+        this.port = port;
+    }
+
+    public boolean readyToQuit(String msg) {
+        return msg.equals(QUIT);
+    }
+
+    private void close(Closeable closeable){
+        if(closeable != null){
+            try {
+                closeable.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void start(){
+        try {
+            client = SocketChannel.open();
+            client.configureBlocking(false);
+
+            selector = Selector.open();
+            client.register(selector, SelectionKey.OP_CONNECT);
+            client.connect(new InetSocketAddress(host,port));
+
+            while (true){
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                for(SelectionKey key : selectionKeys){
+                    handlers(key);
+                }
+                selectionKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }catch (ClosedSelectorException e){
+            //属于用户正常退出
+
+        }finally {
+            close(selector);
+        }
+
+    }
+
+    private void handlers(SelectionKey key) throws IOException {
+        //CONNECT事件，----连接事件
+        if(key.isConnectable()){
+            SocketChannel client = (SocketChannel) key.channel();
+            if(client.isConnectionPending()){
+                client.finishConnect();
+                log.info("客户端【{}】已连接",client.socket().getLocalPort());
+                //处理用户输入
+                new Thread(new UserInputHandler(this)).start();
+            }
+            client.register(selector,SelectionKey.OP_READ);
+        }
+        //READ事件----读事件
+        else if(key.isReadable()){
+            SocketChannel client = (SocketChannel) key.channel();
+            String msg = receive(client);
+            if(StringUtils.isBlank(msg)){
+                //服务器端意外关闭了这个通道
+                close(selector);
+            }else{
+                System.out.println(msg);
+            }
+        }
+    }
+    public void send(String msg)throws  IOException {
+        if (msg.isEmpty()) {
+            return;
+        }
+
+        wBuffer.clear();
+        wBuffer.put(charset.encode(msg));
+        wBuffer.flip();
+        while (wBuffer.hasRemaining()){
+            client.write(wBuffer);
+        }
+
+        if(readyToQuit(msg)){
+            close(selector);
+        }
+
+    }
+    private String receive(SocketChannel client) throws IOException{
+        rBuffer.clear();
+        while (client.read(rBuffer)>0);
+        rBuffer.flip();
+        return String.valueOf(charset.decode(rBuffer));
+    }
+
+    public static void main(String[] args) {
+        ChatClient client = new ChatClient("127.0.0.1",7777);
+        client.start();
+    }
+}
---
+++ src/main/java/com/example/socket/mutileChat/ChatServer.java
@@ -0,0 +1,173 @@
+package com.example.socket.mutileChat;
+
+import esa.commons.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.*;
+import java.nio.charset.Charset;
+import java.util.Set;
+
+/**
+ * 多人聊天室优化版
+ * 使用NIO实现
+ */
+@Slf4j
+public class ChatServer {
+    /***
+     * 服务端的serverSocketChannel
+     */
+    private ServerSocketChannel server;
+    /**
+     * 默认端口号
+     */
+    private static final int DEFAULT_PORT = 8888;
+    /**
+     * 中止通话的字符串
+     */
+    private static final String QUIT = "quit";
+    /**
+     * 默认缓存的大小
+     */
+    private static final int BUFFER = 1024;
+    /**
+     * 选择器，也称为多路复用
+     */
+    private Selector selector;
+    private ByteBuffer rBuffer = ByteBuffer.allocate(BUFFER);
+    private ByteBuffer wBuffer = ByteBuffer.allocate(BUFFER);
+    private Charset charset = Charset.forName("UTF-8");
+
+    private int port;
+
+    public ChatServer() {
+        this(DEFAULT_PORT);
+    }
+
+    public ChatServer(int port) {
+        this.port = port;
+    }
+
+    /**
+     * 核心方法，用来开启服务端
+     */
+    private void start() {
+        try {
+            server = ServerSocketChannel.open();
+            //设置成非阻塞
+            server.configureBlocking(false);
+            //绑定本地端口
+            server.bind(new InetSocketAddress(port));
+            selector = Selector.open();
+            //注册接收事件到选择器上
+            server.register(selector, SelectionKey.OP_ACCEPT);
+            log.info("启动服务器，监听端口:{}...", port);
+            while (true) {
+                selector.select();
+                Set<SelectionKey> selectionKeys = selector.selectedKeys();
+                for (SelectionKey selectionKey : selectionKeys) {
+                    handlers(selectionKey);
+                }
+                selectionKeys.clear();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            close(selector);
+        }
+    }
+
+    private void handlers(SelectionKey selectionKey) throws IOException{
+        //ACCEPT事件--与客户端建立连接
+        if(selectionKey.isAcceptable()){
+            ServerSocketChannel server = (ServerSocketChannel) selectionKey.channel();
+            SocketChannel client = server.accept();
+            client.configureBlocking(false);
+            client.register(selector,SelectionKey.OP_READ);
+            log.info("{}已连接",getClientName(client));
+        }
+        //READ事件，客户端发送了消息
+        else if (selectionKey.isReadable()){
+            SocketChannel client = (SocketChannel) selectionKey.channel();
+            String msg = receive(client);
+
+            if(StringUtils.isEmpty(msg)){
+                //客户端异常
+                selectionKey.cancel();
+                selector.wakeup();
+            }else{
+                log.info("{}: {}",getClientName(client), msg);
+                forwardMessage(client,msg);
+                //检查用户是否退出
+                if(readyToQuit(msg)){
+                    selectionKey.cancel();
+                    selector.wakeup();
+                    log.info("{}已退出",getClientName(client));
+                }
+            }
+
+        }
+
+    }
+
+    private boolean readyToQuit(String msg) {
+        return msg.equals(QUIT);
+    }
+
+    private void forwardMessage(SocketChannel client, String msg) throws IOException {
+        //返回目前注册在selector上的所有channel
+        for(SelectionKey key : selector.keys()){
+            Channel connectedChannel = key.channel();
+            //过滤掉服务端的channel
+            if(connectedChannel instanceof ServerSocketChannel){
+                continue;
+            }
+            //有效的socketChannel
+            if(key.isValid() && !client.equals(connectedChannel)){
+                wBuffer.clear();
+                wBuffer.put(charset.encode(getClientName(client) + ":" + msg));
+                wBuffer.flip();
+                while (wBuffer.hasRemaining()){
+                    ((SocketChannel)connectedChannel).write(wBuffer);
+                }
+            }
+        }
+
+    }
+
+    private String getClientName(SocketChannel client){
+        return "客户端【"+ client.socket().getPort() + "】";
+    }
+
+    private String receive(SocketChannel client) throws IOException {
+        rBuffer.clear();
+        while (client.read(rBuffer) > 0) {
+
+        }
+        rBuffer.flip();
+        return String.valueOf(charset.decode(rBuffer));
+    }
+
+    /**
+     * 关闭所有的流（可用的方法）
+     *
+     * @param closeable
+     */
+    private void close(Closeable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        ChatServer server = new ChatServer(7777);
+        server.start();
+    }
+}
