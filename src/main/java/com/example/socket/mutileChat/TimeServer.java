d753c8742abf0aaa5a0946f481c2e336aad8b1ee
xuemingming <xuemingming@zuowenzhitiao.com>
2021-01-13 20:11:37 +0800

    'fixbug'

---
+++ src/main/java/com/example/socket/mutileChat/TimeServer.java
@@ -0,0 +1,61 @@
+package com.example.socket.mutileChat;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LoggingHandler;
+
+/**
+ * 使用netty实现的服务端
+ */
+public class TimeServer {
+
+
+    public void bind(int port) {
+        //配置服务端NIO线程组
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 1024)
+                    //针对的是bossGroup线程组 主要是应答连接的处理
+                    .handler(new LoggingHandler())
+                    //针对的是workGroup线程组 主要是完成请求后的处理请求逻辑
+                    .childHandler(new ChildChannelHandler());
+            //绑定端口，同步等待成功
+            ChannelFuture f = b.bind(port).sync();
+            //等待服务端监听端口关闭
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }finally {
+            //优雅的退出，释放线程池资源
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+
+    }
+
+    private class ChildChannelHandler extends ChannelInitializer<SocketChannel> {
+        @Override
+        protected void initChannel(SocketChannel socketChannel) throws Exception {
+            socketChannel.pipeline().addLast(new LineBasedFrameDecoder(1024));
+            socketChannel.pipeline().addLast(new StringDecoder());
+            socketChannel.pipeline().addLast(new TimeServerHandler());
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 9999;
+        if(args != null && args.length > 0){
+            port = Integer.parseInt(args[0]);
+        }
+        new TimeServer().bind(port);
+    }
+}
